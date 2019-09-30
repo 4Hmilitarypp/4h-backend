a6699b67caa2c5afbe3f078539cc054d9d2c6f14
Alex <alex@wendte.tech>
2019-09-30 01:01:06 -0500

    adds full integration with aws latestNews

--- server/src/controllers/latestNews.ts
+++ server/src/controllers/latestNews.ts
@@ -1,48 +1,49 @@
-import { Lambda } from 'aws-sdk'
+import apigClientFactory from 'aws-api-gateway-client'
 
 import { Controller } from '../types'
-import { notFoundError } from '../utils/errors'
+import { notFoundError, forbiddenError } from '../utils/errors'
 
-const lambda = new Lambda({ region: 'us-east-1' })
+const apigClient = apigClientFactory.newClient({
+  invokeUrl: 'https://rhcg9ezul9.execute-api.us-east-1.amazonaws.com/dev',
+  region: 'us-east-1',
+  accessKey: process.env.AWS_ACCESS_KEY,
+  secretKey: process.env.AWS_SECRET_ACCESS_KEY,
+})
 
 export const createLatestNews: Controller = async (req, res) => {
+  if (!req.user) throw forbiddenError
   const latestNews = {
     ...req.body,
-    // createdBy: req.user.email,
-    // updatedBy: req.user.email,
+    createdBy: (req.user as any).email,
+    updatedBy: (req.user as any).email,
   }
-  const lambdaResponse = await lambda
-    .invoke({ FunctionName: 'dev-latest-news-write', Payload: JSON.stringify({ body: latestNews }) })
-    .promise()
-  if (!lambdaResponse.Payload) {
+  const lambdaResponse = await apigClient.invokeApi({}, `/latest-news`, 'post', {}, latestNews)
+
+  if (!lambdaResponse.data) {
     return res.status(500).send('lambda error')
   }
-  const latestNewsResponse = JSON.parse(lambdaResponse.Payload.toString())
+  const latestNewsResponse = lambdaResponse.data
   return res.status(201).json(latestNewsResponse)
 }
 
 export const getAllLatestNews: Controller = async (_, res) => {
-  const lambdaResponse = await lambda
-    .invoke({ FunctionName: 'dev-latest-news-read', Payload: JSON.stringify({}) })
-    .promise()
-  if (!lambdaResponse.Payload) {
+  const lambdaResponse = await apigClient.invokeApi({}, `/latest-news`, 'get')
+
+  if (!lambdaResponse.data) {
     return res.status(500).send('lambda error')
   }
-  const allLatestNews = JSON.parse(lambdaResponse.Payload.toString())
+  const allLatestNews = lambdaResponse.data
   return res.json(allLatestNews)
 }
 
 export const getLatestNews: Controller = async (req, res) => {
-  const lambdaResponse = await lambda
-    .invoke({
-      FunctionName: 'dev-latest-news-read',
-      Payload: JSON.stringify({ queryParameters: { slug: req.params.slug } }),
-    })
-    .promise()
-  if (!lambdaResponse.Payload) {
+  const slug = req.params.slug
+  const lambdaResponse = await apigClient.invokeApi({}, `/latest-news/slug/${slug}`, 'get')
+
+  if (!lambdaResponse.data) {
     return res.status(500).send('lambda error')
   }
-  const latestNews = JSON.parse(lambdaResponse.Payload.toString())
+  const latestNews = lambdaResponse.data
   if (!latestNews.length) {
     throw notFoundError
   }
@@ -50,28 +51,23 @@ export const getLatestNews: Controller = async (req, res) => {
 }
 
 export const updateLatestNews: Controller = async (req, res) => {
-  const lambdaResponse = await lambda
-    .invoke({
-      FunctionName: 'dev-latest-news-write',
-      Payload: JSON.stringify({ body: { ...req.body, slug: req.params.slug } }),
-    })
-    .promise()
-  if (lambdaResponse.Payload) {
-    const latestNews = JSON.parse(lambdaResponse.Payload.toString())
+  if (!req.user) throw forbiddenError
+  const id = req.params.id
+  const latestNews = {
+    ...req.body,
+    updatedBy: (req.user as any).email,
+  }
+  const lambdaResponse = await apigClient.invokeApi({}, `/latest-news/${id}`, 'put', {}, latestNews)
+  if (lambdaResponse.data) {
+    const latestNews = lambdaResponse.data
     return res.status(200).json(latestNews)
   }
   return res.status(500).send('lambda error')
 }
 
 export const deleteLatestNews: Controller = async (req, res) => {
-  const lambdaResponse = await lambda
-    .invoke({
-      FunctionName: 'dev-latest-news-write',
-      Payload: JSON.stringify({ queryParameters: { method: 'DELETE', slug: req.params.slug } }),
-    })
-    .promise()
-  if (lambdaResponse.Payload) {
-    return res.status(204).send()
-  }
-  return res.status(500).send('lambda error')
+  const id = req.params.id
+  await apigClient.invokeApi({}, `/latest-news/${id}`, 'delete')
+
+  return res.status(204).send()
 }
--- server/src/controllers/pageInfo.ts
+++ server/src/controllers/pageInfo.ts
@@ -1,18 +1,19 @@
 import { omit, pick } from 'lodash'
 import mongoose from 'mongoose'
 import { Controller } from '../types'
-import { notFoundError } from '../utils/errors'
-import { IPageInfoDocument } from '../models/PageInfo';
+import { notFoundError, forbiddenError } from '../utils/errors'
+import { IPageInfoDocument } from '../models/PageInfo'
 
 const PageInfo = mongoose.model<IPageInfoDocument>('PageInfo')
 
 const cleanPageInfo = (obj: any) => pick(obj, ['info', 'page'])
 
 export const createPageInfo: Controller = async (req, res) => {
+  if (!req.user) throw forbiddenError
   const pageInfo = await new PageInfo({
     ...cleanPageInfo(req.body),
-    createdBy: req.user.email,
-    updatedBy: req.user.email,
+    createdBy: (req.user as any).email,
+    updatedBy: (req.user as any).email,
   }).save()
   return res.status(201).json(pageInfo)
 }
@@ -27,10 +28,11 @@ export const getPageInfo: Controller = async (req, res) => {
 }
 
 export const updatePageInfo: Controller = async (req, res) => {
+  if (!req.user) throw forbiddenError
   const { page } = req.params
   const pageInfo = await PageInfo.findOneAndUpdate(
     page,
-    { ...cleanPageInfo(omit(req.body, 'page')), updatedBy: req.user.email },
+    { ...cleanPageInfo(omit(req.body, 'page')), updatedBy: (req.user as any).email },
     {
       context: 'query',
       new: true,
