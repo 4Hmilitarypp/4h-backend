b9e511723d3157c1fe8b4be19cba34f40574253a
Aaron Simpson <business.feznet@gmail.com>
2020-02-24 18:05:06 -0600

    Format date using new Date() instead of string to fix RangeError bug on editing/viewing camps.

--- client/src/pages/camps/campDates/CampDate.tsx
+++ client/src/pages/camps/campDates/CampDate.tsx
@@ -13,7 +13,10 @@ interface IProps {
 
 const CampDate: React.FC<IProps> = ({ campDate, setModalState, campId }) => (
   <Wrapper onClick={() => setModalState({ action: 'update', campDate, campId })}>
-    <Title>{`${format(campDate.beginDate, 'MMMM D YYYY')} to ${format(campDate.endDate, 'MMMM D YYYY')}`}</Title>
+    <Title>{`${format(new Date(campDate.beginDate), 'MMMM D YYYY')} to ${format(
+      new Date(campDate.endDate),
+      'MMMM D YYYY'
+    )}`}</Title>
   </Wrapper>
 )
 
